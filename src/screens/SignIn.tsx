396200d148ce1fc0c9a811182ed536c4266fbb77
Miguel-Leite <miguelleite200leite@email.com>
2022-11-06 14:51:56 +0100

    :white_check_mark: test context

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,16 @@
 import { Center, Text, Icon } from "native-base";
 import { Fontisto } from '@expo/vector-icons'
 
+import {  useAuth } from '../hooks/useAuth';
+
 import Logo from '../assets/logo.svg';
 import { Button } from "../components/Button";
 
 export function SignIn() {
+  const { user, signIn } = useAuth();
+
+  console.log(`DADOS DO USUÁRIO =>`, user)
+
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
@@ -14,6 +20,7 @@ export function SignIn() {
       type="SECONDARY" 
       leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />} 
       mt={12}
+      onPress={signIn}
       />
       
       <Text color={'white'} textAlign='center' px={5} mt={4}>
