3c47c870f10b2637557eb9494b1d669b518cae59
Miguel-Leite <miguelleite200leite@email.com>
2022-11-06 14:52:27 +0100

    :sparkles: AuthContextProvider imported on app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-import { NativeBaseProvider, StatusBar, Center } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 import { SignIn } from './src/screens/SignIn';
+import { AuthContextProvider } from './src/contexts/AuthContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <SignIn /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
